4928e4069025306f6c7486b1499ef7450cffd81e
et90 <yuriy289@gmail.com>
2024-08-11 15:58:05 +0300

    добавил обводку

--- point.js
+++ point.js
@@ -1,6 +1,7 @@
 export default class point{
     #size = 10;
-    #color = 'red';
+    #colorFill = 'red';
+    #colorStroke = 'black'
     #x = 50;
     #y = 50;
     #r = 10;
@@ -13,9 +14,9 @@ export default class point{
     draw(){
         this.#context.beginPath();
         this.#context.arc(this.#x, this.#y, this.#r, 0, Math.PI * 2, false);
-        this.#context.fillStyle = this.#color;
+        this.#context.fillStyle = this.#colorFill;
         this.#context.fill();
-        this.#context.strokeStyle = 'black';
+        this.#context.strokeStyle = this.#colorStroke;
         this.#context.stroke();
         console.log('draw p')
     }
